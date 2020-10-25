f173563399e308cb2d2ff6abec398292658cb657
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-10-25 23:53:33 +0700

    Menyiapkan admin untuk pengaturan session

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,10 +1,27 @@
-const express = require("express");
 const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const mongoStore = require("connect-mongodb-session")(session);
 
-const Router = express.Router();
+require("dotenv").config();
+const store = new mongoStore({
+  uri: process.env.MONGO_URL,
+  collection: "sess",
+});
 
 const component = path.resolve("components/admin");
 const production = process.env.NODE_ENV === "production";
+
+const Router = express.Router();
+
+Router.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store,
+  })
+);
 
 Router.get("*", (req, res) =>
   !production
