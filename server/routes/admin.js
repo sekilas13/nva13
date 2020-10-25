cdf0e51f6b6ab9924e38e8d2dba0ce58487845cd
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-10-26 06:03:07 +0700

    Menambahkan endpoint login di admin

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const session = require("express-session");
+const { admin: passport } = require("../passport");
 const mongoStore = require("connect-mongodb-session")(session);
 
 require("dotenv").config();
@@ -23,10 +24,35 @@ Router.use(
   })
 );
 
+Router.use(passport.initialize());
+Router.use(passport.session());
+
 Router.get("*", (req, res) =>
   !production
     ? res.redirect("http://localhost:4000")
     : res.sendFile(path.join(component, "index.html"))
 );
+
+Router.post("/login", (req, res, next) => {
+  passport.authenticate("local", function (err, user, info) {
+    if (err) {
+      return res.status(400).json({ errors: err });
+    }
+    if (!user) {
+      return res.status(400).json({ errors: info });
+    }
+    req.logIn(user, function (err) {
+      if (err) {
+        return res.status(400).json({ errors: err });
+      }
+      return res
+        .status(200)
+        .json({
+          success: true,
+          user: { id: user.id, username: user.username },
+        });
+    });
+  })(req, res, next);
+});
 
 module.exports = Router;
