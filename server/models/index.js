c4951a25c1e59e97f0706fc7a7c5f00f85fcb9fe
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-10-26 00:05:53 +0700

    Penambahan user model

---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+const path = require("path");
+
+module.exports = {
+  userAdmin: require(path.join(__dirname, "userAdmin.js")),
+};
---
+++ server/models/userAdmin.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    match: /^[a-zA-Z\s\-]+$/,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+  },
+  password: {
+    required: true,
+  },
+  date: {
+    required: false,
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("useradmin", UserSchema);
