4dc01cfa12a3563e3117019aa6a599f3ccb0421a
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-10-26 20:51:34 +0700

    memperbaiki kesalahan deserializeUser

--- server/passport/admin.js
+++ server/passport/admin.js
@@ -8,7 +8,7 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
+  userAdmin.findById(id, (err, user) => {
     done(err, user);
   });
 });
