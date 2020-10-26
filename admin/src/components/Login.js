c0b5228e506e1480a1ac97703f70800ef2c0d074
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-10-26 13:46:01 +0700

    Rancangan awal untuk login

--- admin/src/components/Login.js
+++ admin/src/components/Login.js
@@ -1,5 +1,35 @@
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+
 function Login() {
-  return <></>;
+  return (
+    <Container>
+      <Row className="justify-content-center mt-4">
+        <Col md={5}>
+          <Card>
+            <Card.Body>
+              <Form>
+                <Form.Group>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" placeholder="Masukan email" />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Kata Sandi</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Masukan kata sandi"
+                  />
+                </Form.Group>
+                <hr />
+                <Button variant="primary" className="btn-block" type="submit">
+                  Login
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Login;
