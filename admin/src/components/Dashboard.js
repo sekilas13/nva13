cdea7aa72c0411dfadf572593cc9aec40aad8061
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-10-28 09:40:55 +0700

    Menambahkan navbar dan mengubah warna <body>

--- admin/src/components/Dashboard.js
+++ admin/src/components/Dashboard.js
@@ -1,5 +1,27 @@
+import { Fragment } from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { useEffect } from "react";
+
 function Dashboard() {
-  return <></>;
+  useEffect(() => {
+    document.querySelector("body").style.backgroundColor = "white";
+  });
+
+  return (
+    <Fragment>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="my-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container></Container>
+    </Fragment>
+  );
 }
 
 export default Dashboard;
