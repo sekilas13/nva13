6daf06f35a69251fef1e9fec1ad4960bb48141c5
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-10-28 23:18:00 +0700

    Membuat folder utils

--- server/passport/index.js
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  admin: require(path.join(__dirname, "admin.js")),
-};
---
+++ server/utils/index.js
@@ -0,0 +1,5 @@
+const path = require("path");
+
+module.exports = {
+  passport: require(path.join(__dirname, "passport.js")),
+};
