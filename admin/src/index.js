8837a9ef62523b4961eaa6f17e03cb593321b1c2
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-10-28 22:35:49 +0700

    Mengahpus provider mobx

--- admin/src/index.js
+++ admin/src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "./utils/stateProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
