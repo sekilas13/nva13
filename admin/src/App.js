cb1b81523993dd42cadd0fe7cd56c1b30af15e5f
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-10-28 22:37:13 +0700

    Memindahkan provider mobx dan menggunakan router dom

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,12 +1,25 @@
-import { useContext } from "react";
-import { Context } from "./utils/stateProvider";
-import { observer } from "mobx-react";
-import { Login, Dashboard } from "./components";
+import { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Provider } from "./utils/stateProvider";
+import { Vote } from "./routes";
 
 function App() {
-  const store = useContext(Context);
-
-  return store.isLogin ? <Dashboard /> : <Login />;
+  return (
+    <Fragment>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Link to="/vote">Go to vote</Link>
+          </Route>
+          <Route path="/vote">
+            <Provider>
+              <Vote />
+            </Provider>
+          </Route>
+        </Switch>
+      </Router>
+    </Fragment>
+  );
 }
 
-export default observer(App);
+export default App;
