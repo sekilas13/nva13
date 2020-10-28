834e4d3e7102810aa67ae5a1867bc9fa4e0fe928
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-10-28 23:22:28 +0700

    Memindahkan dan mengubah nama admin.js menjadi session_store.js

--- server/utils/session_store.js
+++ server/utils/session_store.js
@@ -4,7 +4,7 @@ const mongoStore = require("connect-mongodb-session")(session);
 require("dotenv").config();
 const store = new mongoStore({
   uri: process.env.MONGO_URL,
-  collection: "sess_admin",
+  collection: "sess_store",
 });
 
 module.exports = store;
