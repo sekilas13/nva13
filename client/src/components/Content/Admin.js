2b390cae02269ae66f1a9333569ab8b3f6049e11
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-10-30 21:54:21 +0700

    Menambahkan react-router-dom

--- client/src/components/Content/Admin.js
+++ client/src/components/Content/Admin.js
@@ -1,11 +1,19 @@
 import Swal from "sweetalert2";
-import { Fragment, useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import { Navi } from "../../style/Admin";
 import io from "socket.io-client";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
 import { Context } from "../../utils/stateProvider";
 
 function Admin() {
+  const [toastPop, setToastPop] = useState(false);
   document.querySelector("body").style.backgroundColor = "white";
 
   const store = useContext(Context);
@@ -29,28 +37,38 @@ function Admin() {
       },
     });
 
-    Toast.fire({
-      icon: "success",
-      title: store.isNewLogin ? "Login sukses" : "Selamat datang kembali",
-    });
+    if (!toastPop) {
+      Toast.fire({
+        icon: "success",
+        title: store.isNewLogin ? "Login sukses" : "Selamat datang kembali",
+      });
+      setToastPop(!toastPop);
+    }
   });
 
   socket.on("admin:new user", (d) => console.log(d));
 
+  const { path, url } = useRouteMatch();
+
   return (
-    <Fragment>
+    <Router>
       <Navi variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Admin Panel</Navbar.Brand>
+          <Navbar.Brand as={Link} to={`${url}/`}>
+            Admin Panel
+          </Navbar.Brand>
           <Nav className="my-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link as={Link} to={`${url}/paslon`}>
+              Paslon
+            </Nav.Link>
+            <Nav.Link as={Link} to={`${url}/siswa`}>
+              Siswa
+            </Nav.Link>
           </Nav>
         </Container>
       </Navi>
-      <Container></Container>
-    </Fragment>
+      <Switch></Switch>
+    </Router>
   );
 }
 
