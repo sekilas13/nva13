3023eb87e88b0d1cebf650848eed990dd8c077fd
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-10-30 11:48:09 +0700

    Membuat strategy jwt untuk pengamanan api

---
+++ server/passport/jwt.js
@@ -0,0 +1,21 @@
+const Strategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+const { User } = require("../models");
+
+require("dotenv").config();
+module.exports = (passport) =>
+  passport.use(
+    new Strategy(
+      {
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+        secretOrKey: process.env.JWT_SECRET,
+      },
+      (payload, done) => {
+        return User.findOne({ _id: payload._id })
+          .then((user) => {
+            return done(null, user);
+          })
+          .catch((err) => done(err));
+      }
+    )
+  );
