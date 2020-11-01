0acf1580774447ba32531fe0d9d8d09030bb019b
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-01 09:10:00 +0700

    Update main.js

--- client/src/components/Content/adm/Main.js
+++ client/src/components/Content/adm/Main.js
@@ -1,5 +1,19 @@
+import { Card, Container, Row, Col } from "react-bootstrap";
+
 function Main() {
-  return <p>Path /</p>;
+  return (
+    <Container>
+      <Row className="mt-3">
+        <Col md={4}>
+          <Card className="h-100">
+            <Card.Body>
+              <h2 className="text-center">Catatan User</h2>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Main;
