274f33146a2d93338403398bf225477c55ded625
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-13 23:07:46 +0700

    Menyalakan export untuk admin

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  // admin: require(path.join(__dirname, "admin.js")),
+  admin: require(path.join(__dirname, "admin.js")),
   // siswa: require(path.join(__dirname, "siswa.js")),
   auth: require(path.join(__dirname, "auth.js")),
 };
