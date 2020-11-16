1d7a91af0ad412899981082ea2a6a2470546c190
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-16 13:22:47 +0700

    membuat sub component baru

---
+++ client/src/components/Content/adm/Siswa.js
@@ -0,0 +1,20 @@
+import { useEffect, useContext } from "react";
+import { Context } from "../../../utils/stateProvider";
+import { observer } from "mobx-react";
+import axios from "axios";
+
+function Siswa() {
+  const store = useContext(Context);
+  useEffect(() => {
+    axios
+      .get("/admin", {
+        headers: {
+          Authorization: "Bearer " + store.token,
+        },
+      })
+      .then(console.log);
+  });
+  return <></>;
+}
+
+export default observer(Siswa);
