7f913e2ffeed77fe145c52e07ae8f9a2fb0338a2
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-16 15:11:28 +0700

    Menambahkan autentikasi lapisan login

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,5 +1,12 @@
+const passport = require("passport");
 const express = require("express");
 const Router = express.Router();
+
+Router.use((req, res, next) =>
+  req.isAuthenticated()
+    ? next()
+    : req.json({ err: true, message: "Anda belum login" }).status(401)
+);
 
 Router.get("/", (req, res) => res.json({ msg: "hello" }));
 
