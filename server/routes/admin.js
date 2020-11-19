46ec84686c87a09c528739dc4e2a4ea4ced7395e
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-19 22:38:38 +0700

    Menambahkan autentikasi jwt

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,10 +2,24 @@ const passport = require("passport");
 const express = require("express");
 const Router = express.Router();
 
-Router.use((req, res, next) =>
-  req.isAuthenticated()
-    ? next()
-    : req.json({ err: true, message: "Anda belum login" }).status(401)
+Router.use(
+  (req, res, next) =>
+    req.isAuthenticated()
+      ? next()
+      : req.json({ err: true, message: "Anda belum login" }).status(401),
+  (req, res, next) =>
+    passport.authenticate("jwt", { session: false }, (error, user) => {
+      if (error) {
+        req
+          .json({
+            err: true,
+            message: "Ada kesalahan server, mohon hubungi admin",
+          })
+          .status(503);
+      } else {
+        if (user) next();
+      }
+    })(req, res, next)
 );
 
 Router.get("/", (req, res) => res.json({ msg: "hello" }));
