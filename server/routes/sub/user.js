4e8fb50609babf3de5558013771548a72195ffd9
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-20 08:22:47 +0700

    menambahkan get user siswa endpoint

--- server/routes/sub/user.js
+++ server/routes/sub/user.js
@@ -2,6 +2,15 @@ const express = require("express");
 const { User } = require("../../models");
 const Router = express.Router();
 
-Router.get("/siswa", (req, res) => {});
+Router.get("/siswa", async (req, res) => {
+  try {
+    const siswa = await User.find({ role: "siswa" });
+
+    res.json({ error: false, data: siswa });
+    res.end();
+  } catch (e) {
+    res.json({ error: true, msg: e }).status(503);
+  }
+});
 
 module.exports = Router;
