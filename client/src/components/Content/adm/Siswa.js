41bde79b673074c9083b2d4d1f39eec77ccabacc
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-20 22:06:06 +0700

    Memperbaiki style dan menambahkan tombol tambah

--- client/src/components/Content/adm/Siswa.js
+++ client/src/components/Content/adm/Siswa.js
@@ -1,44 +1,47 @@
 import { Suspense, lazy } from "react";
-import { Container, Row, Col, Table } from "react-bootstrap";
+import { Container, Row, Col, Button, Table } from "react-bootstrap";
 import { observer } from "mobx-react";
 const TableList = lazy(() => import("./splitting/TableList"));
 
 function Siswa() {
   return (
-    <Container>
-      <Row className="mt-3">
-        <Col>
+    <Container fluid>
+      <Row className="mt-3 justify-content-center">
+        <Col md={11}>
           <h1>Daftar Siswa</h1>
         </Col>
       </Row>
-      <Row className="mt-2">
-        <Table bordered>
-          <thead>
-            <tr>
-              <th>Absen</th>
-              <th>Email</th>
-              <th>Nama Lengkap</th>
-              <th>Kelas</th>
-              <th>Tanggal Ditambahkan</th>
-            </tr>
-          </thead>
-          <tbody>
-            <Suspense
-              fallback={
-                <tr>
-                  <td className="text-center" colSpan={2}>
-                    Mohon Tungu
-                  </td>
-                  <td className="text-center" colSpan={2}>
-                    Sedang mengambil data
-                  </td>
-                </tr>
-              }
-            >
-              <TableList />
-            </Suspense>
-          </tbody>
-        </Table>
+      <Row className="justify-content-center mt-2">
+        <Col md={11}>
+          <Button className="mb-2">Tambah</Button>
+          <Table bordered responsive>
+            <thead>
+              <tr>
+                <th>Absen</th>
+                <th>Email</th>
+                <th>Nama Lengkap</th>
+                <th>Kelas</th>
+                <th>Tanggal Ditambahkan</th>
+              </tr>
+            </thead>
+            <tbody>
+              <Suspense
+                fallback={
+                  <tr>
+                    <td className="text-center" colSpan={2}>
+                      Mohon Tungu
+                    </td>
+                    <td className="text-center" colSpan={2}>
+                      Sedang mengambil data
+                    </td>
+                  </tr>
+                }
+              >
+                <TableList />
+              </Suspense>
+            </tbody>
+          </Table>
+        </Col>
       </Row>
     </Container>
   );
