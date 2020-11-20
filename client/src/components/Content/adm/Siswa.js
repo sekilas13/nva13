324170ae67073bf3302b2eef8d43497de059cc7a
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-20 22:48:34 +0700

    Membuat siswa memiliki halaman lain

--- client/src/components/Content/adm/Siswa.js
+++ client/src/components/Content/adm/Siswa.js
@@ -1,50 +1,21 @@
-import { Suspense, lazy } from "react";
-import { Container, Row, Col, Button, Table } from "react-bootstrap";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useRouteMatch,
+} from "react-router-dom";
+import { Main } from "./RouteSiswa";
 import { observer } from "mobx-react";
-const TableList = lazy(() => import("./splitting/TableList"));
 
 function Siswa() {
+  const { path } = useRouteMatch();
+
   return (
-    <Container fluid>
-      <Row className="mt-3 justify-content-center">
-        <Col md={11}>
-          <h1>Daftar Siswa</h1>
-        </Col>
-      </Row>
-      <Row className="justify-content-center mt-2">
-        <Col md={11}>
-          <Button className="mb-2">Tambah</Button>
-          <Table bordered responsive>
-            <thead>
-              <tr>
-                <th>Absen</th>
-                <th>Email</th>
-                <th>Nama Lengkap</th>
-                <th>Kelas</th>
-                <th>Tanggal Ditambahkan</th>
-                <th>Aksi</th>
-              </tr>
-            </thead>
-            <tbody>
-              <Suspense
-                fallback={
-                  <tr>
-                    <td className="text-center" colSpan={3}>
-                      Mohon Tungu
-                    </td>
-                    <td className="text-center" colSpan={3}>
-                      Sedang mengambil data
-                    </td>
-                  </tr>
-                }
-              >
-                <TableList />
-              </Suspense>
-            </tbody>
-          </Table>
-        </Col>
-      </Row>
-    </Container>
+    <Router>
+      <Switch>
+        <Route exact path={path} component={Main} />
+      </Switch>
+    </Router>
   );
 }
 
