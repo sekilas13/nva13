b3bdfb77920a85baea76545b1a3395b361878e2c
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-20 22:48:06 +0700

    Membuat folder untuk subrouting siswa

---
+++ client/src/components/Content/adm/RouteSiswa/Main.js
@@ -0,0 +1,51 @@
+import { Suspense, lazy } from "react";
+import { Container, Row, Col, Button, Table } from "react-bootstrap";
+import { observer } from "mobx-react";
+const TableList = lazy(() => import("../splitting/TableList"));
+
+function Main() {
+  return (
+    <Container fluid>
+      <Row className="mt-3 justify-content-center">
+        <Col md={11}>
+          <h1>Daftar Siswa</h1>
+        </Col>
+      </Row>
+      <Row className="justify-content-center mt-2">
+        <Col md={11}>
+          <Button className="mb-2">Tambah</Button>
+          <Table bordered responsive>
+            <thead>
+              <tr>
+                <th>Absen</th>
+                <th>Email</th>
+                <th>Nama Lengkap</th>
+                <th>Kelas</th>
+                <th>Tanggal Ditambahkan</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <Suspense
+                fallback={
+                  <tr>
+                    <td className="text-center" colSpan={3}>
+                      Mohon Tungu
+                    </td>
+                    <td className="text-center" colSpan={3}>
+                      Sedang mengambil data
+                    </td>
+                  </tr>
+                }
+              >
+                <TableList />
+              </Suspense>
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default observer(Main);
