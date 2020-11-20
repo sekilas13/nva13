c29c2664637a8afe489d46a20ab49f264deb25d7
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-20 13:45:23 +0700

    Menambahkan prototipe tabel data siswa

--- client/src/components/Content/adm/Siswa.js
+++ client/src/components/Content/adm/Siswa.js
@@ -1,4 +1,5 @@
 import { useEffect, useContext } from "react";
+import { Container, Row, Col, Table } from "react-bootstrap";
 import { Context } from "../../../utils/stateProvider";
 import { observer } from "mobx-react";
 import axios from "axios";
@@ -14,7 +15,27 @@ function Siswa() {
       })
       .then(console.log);
   });
-  return <></>;
+  return (
+    <Container>
+      <Row className="mt-3">
+        <Col>
+          <h1>Data Siswa</h1>
+        </Col>
+      </Row>
+      <Row className="mt-2">
+        <Table bordered>
+          <thead>
+            <tr>
+              <th>Absen</th>
+              <th>Nama Lengkap</th>
+              <th>Kelas</th>
+              <th>Tanggal Ditambahkan</th>
+            </tr>
+          </thead>
+        </Table>
+      </Row>
+    </Container>
+  );
 }
 
 export default observer(Siswa);
