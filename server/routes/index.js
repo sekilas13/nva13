6bb90d7054b320401f75a26ad2637a813f4eca86
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-20 19:42:28 +0700

    Membuat export lebih mudah dibaca

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,5 @@
-const path = require("path");
-
 module.exports = {
-  admin: require(path.join(__dirname, "admin.js")),
-  // siswa: require(path.join(__dirname, "siswa.js")),
-  auth: require(path.join(__dirname, "auth.js")),
+  admin: require("./admin"),
+  // siswa: require("./siswa"),
+  auth: require("./auth"),
 };
