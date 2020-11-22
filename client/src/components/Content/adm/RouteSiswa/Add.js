44c6bb6ca07b5073277c5ab55ba539abf33511e7
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-22 17:24:28 +0700

    Menambahkan tampilan dasar untuk menambahkan siswa

--- client/src/components/Content/adm/RouteSiswa/Add.js
+++ client/src/components/Content/adm/RouteSiswa/Add.js
@@ -1,5 +1,77 @@
+import { useHistory } from "react-router-dom";
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+
 function Add() {
-  return <>Halaman Tambah</>;
+  const history = useHistory();
+
+  return (
+    <Container>
+      <Row className="justify-content-center mt-3">
+        <Col md={8}>
+          <Card>
+            <Card.Body>
+              <Row className="justify-content-center">
+                <Col md={6}>
+                  <h4 className="text-center">Tambahkan Data Siswa</h4>
+                </Col>
+              </Row>
+              <Row className="justify-content-center">
+                <Col md={10}>
+                  <Form>
+                    <Form.Group>
+                      <Form.Label>Nama Lengkap</Form.Label>
+                      <Form.Control
+                        name="username"
+                        type="input"
+                        placeholder="Masukan nama lengkap siswa"
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Label>Email</Form.Label>
+                      <Form.Control
+                        name="email"
+                        type="email"
+                        placeholder="Masukan email siswa"
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Label>Absen</Form.Label>
+                      <Form.Control
+                        name="absen"
+                        type="number"
+                        placeholder="Masukan nomor absen siswa"
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlSelect1">
+                      <Form.Label>Pilih Kelas</Form.Label>
+                      <Form.Control as="select">
+                        <option value="">Pilih kelas</option>
+                      </Form.Control>
+                    </Form.Group>
+                    <Button
+                      variant="primary"
+                      className="btn-block"
+                      type="submit"
+                    >
+                      Login
+                    </Button>
+                  </Form>
+                </Col>
+              </Row>
+              <Row className="justify-content-center mt-2">
+                <Button
+                  variant="outline-success"
+                  onClick={() => history.push("/vote/siswa")}
+                >
+                  Kembali
+                </Button>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Add;
