8ecd86eb8e6cd87981ed3da0e291e984c411688a
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-05 12:50:43 +0700

    Memperbarui server.js

--- server.js
+++ server.js
@@ -8,17 +8,28 @@ const path = require("path");
 
 const app = express();
 
+const { fullUrl } = require("./utils");
+const nm_dir = path.join(__dirname, "node_modules");
+const pub_dir = path.join(__dirname, "public");
 const PORT = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(pub_dir));
+
 app.use(noCache());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(favicon(path.join(__dirname, "public/favicon.ico")));
+app.use(favicon(path.join(pub_dir, "favicon.ico")));
+app.use("/bs", express.static(path.join(nm_dir, "bootstrap/dist")));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) =>
+  res.render("pages/landingPage", {
+    cannonical: fullUrl(req),
+    title: "Pilih Paslon Mu | NVA13",
+  })
+);
 
 const server = http.createServer(app);
 
