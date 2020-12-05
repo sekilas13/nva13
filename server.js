b202880d5fcb6db8c256b4f9404135602d5eea1b
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-05 11:55:08 +0700

    Menambahkan fungsi untuk favicon dan set view engine

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const bodyParser = require("body-parser");
+const favicon = require("serve-favicon");
 const socketIO = require("socket.io");
 const noCache = require("nocache");
 const express = require("express");
 const http = require("http");
+const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use(noCache());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(favicon(path.join(__dirname, "public/favicon.ico")));
 
 app.get("/", (req, res) => res.send("Hello World"));
 
