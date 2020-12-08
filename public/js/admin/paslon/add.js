6256fdf3ac4ac218fc681187c86762fe3b233994
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-08 19:02:08 +0700

    Menambahkan fungsi validasi

--- public/js/admin/paslon/add.js
+++ public/js/admin/paslon/add.js
@@ -1,6 +1,56 @@
+const required = "Bidang ini harus diisi !";
+const minLength = "Nama minimal harus memiliki panjang 3 kata !";
+
+$.validator.addMethod(
+  "regex",
+  function (value, element, regexp) {
+    var re = new RegExp(regexp);
+    return this.optional(element) || re.test(value);
+  },
+  "Please check your input."
+);
+
 $(function () {
   $("#gambarPaslon").on("change", function () {
     const fileName = $(this).val().replace(/^.*\\/, "");
     $(this).next(".custom-file-label").html(fileName);
+  });
+
+  $("#paslon-add").validate({
+    rules: {
+      nameKetua: {
+        required: true,
+        minLength: 3,
+        regex: /^[a-zA-Z\s\-]+$/,
+      },
+      nameWaketu: {
+        required: true,
+        minLength: 3,
+        regex: /^[a-zA-Z\s\-]+$/,
+      },
+      gambarPaslon: {
+        required: true,
+      },
+    },
+    messages: {
+      nameKetua: {
+        required,
+        regex: "Nama harus berupa huruf kecil atau kapital !",
+      },
+      nameWaketu: {
+        required,
+        regex: "Nama harus berupa huruf kecil atau kapital !",
+      },
+      gambarPaslon: {
+        required: "Pilih gambar terlebih dahulu !",
+      },
+    },
+    submitHandler: function (form) {
+      form.submit();
+    },
+    errorPlacement: function (label, element) {
+      label.addClass("invalid-feedback");
+      label.insertAfter(element);
+    },
   });
 });
