fbbd716f532114e6885c0fd29cc1f0718c8985fb
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-08 11:36:03 +0700

    Menambahkan notice tidak ada data di landingPage dan membuat render lebih singkat

--- routes/admin/paslon.js
+++ routes/admin/paslon.js
@@ -4,13 +4,13 @@ const express = require("express");
 const Router = express.Router();
 
 Router.get("/", (req, res) =>
-  Paslon.find({}).then((data) => {
-    return res.render("pages/admin/paslon/read", {
+  Paslon.find({}).then((data) =>
+    res.render("pages/admin/paslon/read", {
       title: "Daftar Paslon | NVA13",
       cannonical: fullUrl(req),
       data,
-    });
-  })
+    })
+  )
 );
 
 module.exports = Router;
--- routes/main.js
+++ routes/main.js
@@ -1,13 +1,17 @@
+const { Paslon } = require("../models");
 const express = require("express");
 const Router = express.Router();
 
 const { fullUrl } = require("../utils");
 
 Router.get("/", (req, res) =>
-  res.render("pages/landingPage", {
-    cannonical: fullUrl(req),
-    title: "Pilih Paslon Mu | NVA13",
-  })
+  Paslon.find({}).then((data) =>
+    res.render("pages/landingPage", {
+      cannonical: fullUrl(req),
+      title: "Pilih Paslon Mu | NVA13",
+      data,
+    })
+  )
 );
 
 module.exports = Router;
