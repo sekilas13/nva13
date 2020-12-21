0e0f3871333a58cf8e9f0e7bac7d2d17c47d368d
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-21 19:56:47 +0700

    Menambahkan JSDoc

--- utils/AuthCheker.js
+++ utils/AuthCheker.js
@@ -1,3 +1,11 @@
+/**
+ * Function untuk mengecek apakah user sudah login terlebih dahulu
+ * @module express
+ * @function checkAuthenticated
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ * @param {Function} next - Express next middleware function
+ */
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
@@ -5,6 +13,14 @@ function checkAuthenticated(req, res, next) {
   res.redirect("/admin/login");
 }
 
+/**
+ * Function untuk mengecek apakah user belum ada status login
+ * @module express
+ * @function checkNotAuthenticated
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ * @param {Function} next - Express next middleware function
+ */
 function checkNotAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return res.redirect("/admin");
--- utils/fullUrl.js
+++ utils/fullUrl.js
@@ -1,2 +1,9 @@
+/**
+ * Function untuk mendapatkan full Url
+ * @module express
+ * @function fullUrl
+ * @param {object} req Express Request Type
+ * @return String
+ */
 module.exports = (req) =>
   req.protocol + "://" + req.get("host") + req.originalUrl;
